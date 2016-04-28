3b2ee08a603124f4f843d7de7ef4bde6d7bae3d0
joaofelipe1294 <joaofelipe1294@gmail.com>
2016-04-27 23:48:17 -0300

    Adicionada classe que prepara a exibicao de todas as empresas cadastradas

---
+++ DOR/src/java/tarefas/PreparaListarEmpresas.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarefas;
+
+import dao.EmpresaDAO;
+import interfaces.Tarefa;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import modelos.Empresa;
+
+/**
+ *
+ * @author joaolopes
+ */
+public class PreparaListarEmpresas implements Tarefa{
+
+    @Override
+    public String executa(HttpServletRequest req, HttpServletResponse resp) {
+        List<Empresa> listaEmpresas = new EmpresaDAO().lista();
+        HttpSession sessao = req.getSession();
+        sessao.setAttribute("lista_empresas", listaEmpresas);
+        return "paginas_protegidas/empresa/empresas.jsp";
+    }
+    
+}
